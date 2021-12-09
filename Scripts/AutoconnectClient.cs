05423b2b71effc652ca097119f2765512062a02e
Erik Herrmann <Erik.Herrmann@dfki.de>
2021-12-09 13:31:44 +0100

    Added support for config file to AutoconnectClient

--- Scripts/AutoconnectClient.cs
+++ Scripts/AutoconnectClient.cs
@@ -1,13 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.IO;
 using UnityEngine;
 using Mirror;
+using kcp2k;
 
+
+[Serializable]
+public class ClientConfig
+{
+    public string url = "localhost";
+    public int port = 7777;
+}
+
+[RequireComponent(typeof(NetworkManager))]
 public class AutoconnectClient : MonoBehaviour
 {
-    public NetworkManager networkManager;
+    NetworkManager networkManager;
+    KcpTransport transport;
+    public ClientConfig config;
     void Start()
     {
+
+        networkManager = GetComponent<NetworkManager>();
+        transport = GetComponent<KcpTransport>();
+        string configFile = Path.Combine(Application.streamingAssetsPath, "config.json");
+        string configText = File.ReadAllText(configFile);
+        config = JsonUtility.FromJson<ClientConfig>(configText);
+     
+        networkManager.networkAddress = config.url;
+        transport.Port = (ushort)config.port;
         networkManager.StartClient();
     }
 
