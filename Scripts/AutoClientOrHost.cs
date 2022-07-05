b11700176423318e9ebfc1b00e59404ad89bdf8f
Erik Herrmann <Erik.Herrmann@dfki.de>
2022-07-05 19:04:50 +0200

    Modified auto client or host script to load the trigger the additive loading of the server scene

--- Scripts/AutoClientOrHost.cs
+++ Scripts/AutoClientOrHost.cs
@@ -8,13 +8,17 @@ public class AutoClientOrHost : MonoBehaviour
     public bool host;
     public bool server;
     public bool client;
+    public string sceneName;
     // Start is called before the first frame update
     void Start()
     {
         var n = GetComponent<NetworkManager>();
         if(client)n.StartClient();
         if(host)n.StartHost();
-        if(server)n.StartServer();
+        if(server){
+            n.StartServer();
+            NetworkManager.singleton.ServerChangeScene(sceneName);
+        }
     }
 
     // Update is called once per frame
